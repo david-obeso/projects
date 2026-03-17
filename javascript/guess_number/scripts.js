4fad78c4bf396110ff10c7bc5a30922e22e2c714
david-obeso <dobeso@gmail.com>
2026-03-17 17:05:42 +0100

    guess number game improved

--- javascript/guess_number/scripts.js
+++ javascript/guess_number/scripts.js
@@ -3,18 +3,46 @@ document.addEventListener("DOMContentLoaded", function () {
     let feedbackMessage = document.getElementById("feedback-message");
     let secretAnswer = Math.floor(Math.random() * 10) + 1;
     let button = document.getElementById("guess-btn");
+    let userGuess = document.getElementById("user-guess");
+    let attempts = 0;
+    let attemptsElement = document.getElementById("attempts");
 
     button.addEventListener("click", function () {
-        let userGuessInput = document.getElementById("user-guess").value;
-
-
-
-        if (Number(userGuessInput) === secretAnswer) {
+        document.body.classList.remove("win");
+        document.body.classList.remove("lose");
+        let userGuessInput = Number(userGuess.value);
+        if (userGuessInput < 1 || userGuessInput > 10) {
+            feedbackMessage.textContent = "Please enter a number between 1 and 10";
+            return;
+        }
+        attempts++;
+        attemptsElement.textContent = `Attempts: ${attempts}`;
+        feedbackMessage.textContent = "";
+        // check if they won
+        if (userGuessInput === secretAnswer) {
             feedbackMessage.textContent = "You win!";
+            document.body.classList.add("win");
+            resetGame();
+            // check if they run out of attempts (lost)       
+        } else if (attempts === 3) {
+            feedbackMessage.textContent = `You lose! The number was ${secretAnswer}`;
+            document.body.classList.add("lose");
+            resetGame();
+            // give hints if they didn't win or lose
         } else if (userGuessInput < secretAnswer) {
             feedbackMessage.textContent = "Too low!";
         } else {
             feedbackMessage.textContent = "Too high!";
         }
     });
+    userGuess.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            button.click();
+        }
+    });
+    function resetGame() {
+        secretAnswer = Math.floor(Math.random() * 10) + 1;
+        userGuess.value = "";
+        attempts = 0;
+    }
 });
